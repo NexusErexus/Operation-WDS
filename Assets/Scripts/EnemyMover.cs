eb0af8020855ba17a56d0b4e6c240982b81f82f9
NexusErexus <uksus924@gmail.com>
2024-10-10 22:43:00 +0300

    added smooth rotation and clickable tiles

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float enemySpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,39 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (var waypoint in path)
         {
-            //Debug.Log(waypoint.name);
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float timePercent = 0f;
+            
+            Vector3 directionTarget = endPosition - startPosition;
+            Quaternion startRotation = transform.rotation;
+            Quaternion endRotation = Quaternion.LookRotation(directionTarget);
+
+            while (timePercent <= 1f)
+            {
+                
+                /*if (timePercent < 0.25f)
+                {
+                    float rotationAngle = 4 * rotation * Time.deltaTime * enemySpeed;
+                    transform.Rotate(new Vector3(0, rotationAngle, 0));
+                }*/
+            
+                timePercent += Time.deltaTime * enemySpeed;
+                //Debug.Log(timePercent);
+                transform.SetPositionAndRotation(Vector3.Lerp(startPosition, endPosition, timePercent), Quaternion.Slerp(startRotation, endRotation, 5 * timePercent));
+                yield return new WaitForEndOfFrame();
+            }
+            
+            
         }
         
     }
+
+    /*float RotationProcess(Vector3 currentDir, Vector3 startPosition, Vector3 endPosition)
+    {
+        Vector3 finalDirection = endPosition - startPosition;
+        return Vector3.SignedAngle(currentDir, finalDirection, Vector3.up);
+    }*/
 
     
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlaceable;
+    public InputActionReference mouseClickAction;
+    //CoordinateLabel coordinateLabel = new CoordinateLabel();
+
+    private void OnEnable()
     {
-        
+        mouseClickAction.action.performed += OnMouseClickAction;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        mouseClickAction.action.performed -= OnMouseClickAction;
+    }
+
+    public void OnMouseClickAction(InputAction.CallbackContext cxt)
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform == transform && isPlaceable)
+            {
+                Debug.Log(transform.name);
+            }
+        }
         
     }
 }
